eec7058a248bd5a3f9ed5684a6b4564ed7805419
Anass AZIZI ALAOUI <anass.azizi@nemo.ma>
2026-02-04 18:53:10 +0100

    fix: test

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,6 +4,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173
-  }
+    port: 5173,
+  },
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./test/setup.ts",
+  },
 });
